0788483233462d5c81f41255c96c4f71ab8948f3
Julian Papesch <jpapesch000@gmail.com>
2025-01-24 23:18:57 +0100

    Player Movement and Bounce Added

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,65 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct PlayerMoveData
+{
+    public float verticalAccel;
+    public float horizontalAccel;
+    public float maxVerticalVel;
+    public float maxHorizontalVel;
+
+    public float drag;
+
+    public float bounceFactor;
+}
+public class Player : MonoBehaviour
+{
+    public int playerIndex;
+
+    #region Movement
+
+    [SerializeField] private PlayerMoveData moveData;
+    private PlayerMovement movement;
+    private Rigidbody rb;
+
+    #endregion
+
+    private void OnEnable()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        movement = new();
+        movement.Initialize(playerIndex, moveData, rb);
+    }
+
+    private void FixedUpdate()
+    {
+        movement.Update();
+
+        foreach (var joystick in Input.GetJoystickNames())
+        {
+            Debug.Log("Joystick connected: " + joystick);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        List<Vector3> contactPoints = new();
+
+        foreach(ContactPoint contact in collision.contacts)
+            contactPoints.Add(contact.point);
+
+        Vector3 sum = Vector3.zero;
+
+        foreach(Vector3 point in contactPoints)
+            sum += point;
+
+        Vector3 averageContactPoint = sum / contactPoints.Count;
+
+        Vector3 contactPointToThis = (transform.position - averageContactPoint).normalized;
+
+        rb.linearVelocity += contactPointToThis * moveData.bounceFactor;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,65 @@
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerMovement
+{
+    private PlayerMoveData moveData;
+    private int playerIndex;
+    private Rigidbody rb;
+    public void Initialize(int index, PlayerMoveData pMoveData, Rigidbody rigidbody)
+    {
+        moveData = pMoveData;
+        playerIndex = index;
+        rb = rigidbody;
+    }
+    public void Update()
+    {
+        KeyCode buttonA     = (KeyCode)System.Enum.Parse(typeof(KeyCode), $"Joystick{playerIndex}Button0");
+        KeyCode buttonB     = (KeyCode)System.Enum.Parse(typeof(KeyCode), $"Joystick{playerIndex}Button1");
+        KeyCode buttonX     = (KeyCode)System.Enum.Parse(typeof(KeyCode), $"Joystick{playerIndex}Button2");
+        KeyCode buttonY     = (KeyCode)System.Enum.Parse(typeof(KeyCode), $"Joystick{playerIndex}Button3");
+        KeyCode buttonLB    = (KeyCode)System.Enum.Parse(typeof(KeyCode), $"Joystick{playerIndex}Button4");
+        KeyCode buttonRB    = (KeyCode)System.Enum.Parse(typeof(KeyCode), $"Joystick{playerIndex}Button5");
+        KeyCode buttonBack  = (KeyCode)System.Enum.Parse(typeof(KeyCode), $"Joystick{playerIndex}Button6");
+        KeyCode buttonStart = (KeyCode)System.Enum.Parse(typeof(KeyCode), $"Joystick{playerIndex}Button7");
+        KeyCode buttonL3    = (KeyCode)System.Enum.Parse(typeof(KeyCode), $"Joystick{playerIndex}Button8");
+        KeyCode buttonR3    = (KeyCode)System.Enum.Parse(typeof(KeyCode), $"Joystick{playerIndex}Button9");
+        KeyCode buttonGuide = (KeyCode)System.Enum.Parse(typeof(KeyCode), $"Joystick{playerIndex}Button10");
+
+        string leftStickHorizontal = $"Horizontal{playerIndex}";
+        string leftStickVertical = $"Vertical{playerIndex}";
+
+        float horizontalInput = Input.GetAxis(leftStickHorizontal);
+        float verticalInput = Input.GetAxis(leftStickVertical);
+
+        if ((horizontalInput >= 0 && rb.linearVelocity.x < moveData.maxHorizontalVel) || 
+            (horizontalInput <= 0 && rb.linearVelocity.x > -moveData.maxHorizontalVel))
+        {
+            rb.linearVelocity += new Vector3( horizontalInput * moveData.horizontalAccel, 0, 0 );
+        }
+
+        if ((verticalInput >= 0 && rb.linearVelocity.y < moveData.maxVerticalVel) || 
+            (verticalInput <= 0 && rb.linearVelocity.y > -moveData.maxVerticalVel))
+        {
+            rb.linearVelocity += new Vector3( 0, verticalInput * moveData.verticalAccel, 0 );
+        }
+
+        if (rb.linearVelocity.x > 0)
+        {
+            rb.linearVelocity = new Vector3(Mathf.Max(0, rb.linearVelocity.x - moveData.drag * Time.deltaTime), rb.linearVelocity.y, rb.linearVelocity.z);
+        }
+        else if (rb.linearVelocity.x < 0)
+        {
+            rb.linearVelocity = new Vector3(Mathf.Min(0, rb.linearVelocity.x + moveData.drag * Time.deltaTime), rb.linearVelocity.y, rb.linearVelocity.z);
+        }
+
+        if (rb.linearVelocity.y > 0)
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, Mathf.Max(0, rb.linearVelocity.y - moveData.drag * Time.deltaTime), rb.linearVelocity.z);
+        }
+        else if (rb.linearVelocity.y < 0)
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, Mathf.Min(0, rb.linearVelocity.y + moveData.drag * Time.deltaTime), rb.linearVelocity.z);
+        }
+    }
+}
